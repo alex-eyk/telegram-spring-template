018e3176b9e27d389bf2e85bd18326b5b2f6c6a1
Alexey Kiselev <ximand931@gmail.com>
2022-03-26 10:19:32 +0300

    initial commit

---
+++ src/main/kotlin/com/alex/eyk/telegram/telegram/TelegramSpringTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.alex.eyk.telegram.telegram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TelegramSpringTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<TelegramSpringTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/alex/eyk/telegram/telegram/TelegramSpringTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alex.eyk.telegram.telegram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TelegramSpringTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
